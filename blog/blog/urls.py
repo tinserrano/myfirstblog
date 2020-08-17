4f931e2a59e632721f6c6002cb30676a7a9339e2
martinpenas <47615086+tinserrano@users.noreply.github.com>
2020-08-17 16:23:32 -0300

    Agregadas vistas y plantilla para el detalle del post

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from . import urls
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
